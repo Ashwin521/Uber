119640e192ab1635b5475e921c95234241333a2a
Ashwin <ashwingiri521@gmail.com>
2025-07-28 12:38:35 +0530

    user authentication done

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const express = require("express");
 const app = express();
@@ -7,7 +8,7 @@ const userRoutes = require("./routes/user.routes");
 const connectToDb = require("./db/db");
 
 app.use(cors());
-
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
